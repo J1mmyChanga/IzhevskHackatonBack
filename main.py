3c971872f63afe28b1fa4b6e80915d71c8e4933f
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2023-09-17 18:51:55 +0300

    tours logic

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ async def index(request: Request):
 
 @app.get("/tours", response_class=HTMLResponse)
 async def tours(request: Request):
-    params = {'request': request, 'title': 'Туры'}
+    user_tours = []
+    of_tours = []
+    tours = []
+    for i in tours:
+        if i.flag == True:
+            user_tours.append(i)
+            continue
+        of_tours.append(i)
+    user_tours = [user_tours[i:i + 3] for i in range(0, len(user_tours), 3)]
+    of_tours = [of_tours[i:i + 3] for i in range(0, len(of_tours), 3)]
+    tours = [tours[i:i + 3] for i in range(0, len(tours), 3)]
+    params = {'request': request, 'title': 'Туры', 'user_tours': user_tours, 'of_tours': of_tours, 'tours': tours}
     requests.get("http://127.0.0.1:8000/api/getLooks/").json()
     return templates.TemplateResponse("tours.html", params)
