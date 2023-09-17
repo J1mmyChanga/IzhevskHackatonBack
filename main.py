b4830ea700f0d9e27f1d67fa49b6fb3b557d4c02
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2023-09-17 16:50:12 +0300

    added index and tour pages

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    params = {'request': request, 'title': 'Главная'}
+    return templates.TemplateResponse("index.html", params)
+
+
+@app.get("/tours", response_class=HTMLResponse)
+async def tours(request: Request):
+    params = {'request': request, 'title': 'Туры', "message": f"Hello {'Туры'}"}
+    return templates.TemplateResponse("tours.html", params)
